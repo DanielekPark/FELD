d7a3d2e6df3228e9c96c5bec6c63f450c05b8615
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2024-03-08 16:02:02 -0800

    feat: refactored api routes and component to use cockroachdb

--- pages/api/results/[id].ts
+++ pages/api/results/[id].ts
@@ -1,41 +1,32 @@
-const mysql = require('mysql')
+const { Client } = require("pg");
 
-export default function handler(req: any, res: any) {
-  type Data = {
-    name: string
-  }
-
-  const connection = mysql.createConnection(process.env.DATABASE_URL)
-
-  connection.connect(function (err: any) {
-    if (err) throw err
-    const { query, method } = req
+export default async function handler (req: any, res: any) {
+  const client = new Client(process.env.DATABASE_URL);
+  try {
+    const {query} = req
     const values = query.id.split(' ')
     const tagsLength = values.length
     const searchTags =
       tagsLength <= 1
-        ? values.map((tag: string) => `"%${tag}%"`).join('')
+        ? values.map((tag: string) => `'%${tag}%'`).join('')
         : values
           .map((item: string, index: number) => {
             if (index === 0) {
-              return `"%${item}%" or `
+              return `'%${item}%' or `
             }
             if (index === values.length - 1) {
-              return `tags like "%${item}%"`
+              return `tags like '%${item}%'`
             }
-            return `tags like "%${item}%" or `
+            return `tags like '%${item}%' or `
           })
-          .join('')
-
-    connection.query(
-      `SELECT * FROM links WHERE tags like ${searchTags}`,
-      function (err: any, result: any, fields: any) {
-        // console.log('search results:', searchTags)
-        if (err) throw err
-        res.status(200).json(result)
-      }
-    )
-  })
-
-  // connection.end();
+          .join('')   
+    await client.connect();
+    const results = await client.query(`SELECT name, link, details, id from links where tags like ${searchTags}`);
+    res.status(200).json(results.rows)
+  } catch (err) {
+    console.log("error executing query:", err);
+  } finally{
+    await client.end() 
+  }
+  
 }
--- pages/results/[id].tsx
+++ pages/results/[id].tsx
@@ -35,7 +35,7 @@ const SearchLinks = () => {
           <div className='relative max-w-7xl mx-auto'>
             <div className='mt-12 max-w-lg mx-auto grid gap-5 md:grid-cols-2 lg:grid-cols-3 lg:max-w-none'>
               {data?.map((item: any, index: number) => {
-                const { name, img, details, link } = item
+                const { name, details, link } = item
                 return (
                   <Card name={name} details={details} link={link} key={`${index}${name}`} />
                 )
